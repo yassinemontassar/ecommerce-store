2d169de751fcb7f6741101a2378cee88baa3fa86
yassinemontassar <sawthegamer70@gmail.com>
2023-12-19 22:31:20 +0100

    responsive update

---
+++ components/HamburgerMenu.tsx
@@ -0,0 +1,113 @@
+
+"use client";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import { Menu, Transition } from "@headlessui/react";
+import { Fragment, useState } from "react";
+import { Category } from "@/types";
+import { cn } from "@/lib/utils";
+import {SearchIcon } from "lucide-react";
+interface MainNavProps {
+  data: Category[];
+}
+
+const HamburgerMenu: React.FC<MainNavProps> = ({ data }) => {
+    const [searchQuery, setSearchQuery] = useState('');
+    const router = useRouter();
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setSearchQuery(e.target.value);
+    };
+
+    const handleSearchClick = () => {
+        performSearch();
+      };
+    
+      const performSearch = () => {
+    
+          const encodedQuery = encodeURIComponent(searchQuery);
+          router.push(`products?name=${encodedQuery}`);
+          setSearchQuery('');
+      };
+
+      
+
+  const pathname = usePathname();
+
+  const routes = data.map((route) => ({
+    href: `/category/${route.id}`,
+    label: route.name,
+    active: pathname === `/category/${route.id}`,
+  }));
+
+  return (
+    <div className="block rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75 z-[100] md:hidden">
+      <Menu as="div" className="relative inline-block text-left">
+        <div>
+          <Menu.Button>
+          <span  className="sr-only">Toggle menu</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="2"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+          </svg>
+
+          </Menu.Button>
+        </div>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
+          <p className="flex flex-col items-center text-xl text-zinc-800 bg-slate-200">Categories:</p>
+            <div className="flex flex-col items-center px-1 py-1">
+              {routes.map((route) => (
+                <Menu.Item key={route.href}>
+                
+                    <div>
+                      <Link
+                        key={route.href}
+                        href={route.href}
+                        className={cn(
+                          " text-sm font-medium transition-colors hover:text-black",
+                          route.active ? "text-black " : "text-neutral-500 "
+                        )}
+                      >
+                        {route.label}
+                      </Link>
+                    </div>
+
+                </Menu.Item>
+              ))}
+            </div>  
+            <p className="flex flex-col items-center text-xl text-zinc-800 bg-slate-200">Search</p>
+            <div className="flex flex-col items-center px-1 py-1">
+                <div className="flex ml-auto  items-center">
+                <input 
+                  type="text"
+                  placeholder="Search..."
+                  value={searchQuery}
+                  onChange={handleInputChange}
+                className="flex items-center border border-gray-300 px-1 py-1 w-40  rounded-full focus:outline-none focus:ring focus:border-blue-300"
+                />
+                 <SearchIcon
+                      onClick={handleSearchClick}
+                 />
+            </div>  
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
+    </div>
+  );
+};
+export default HamburgerMenu;
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,10 @@
-import Link from "next/link";
-
+import Image from "next/image";
 import MainNav from "@/components/main-nav";
 import Container from "@/components/ui/container";
 import getCategories from "@/actions/get-categories";
 import NavbarActions from "@/components/narvbar-actions";
 import SearchBar from "@/components/search"
-
+import HamburgerMenu from "./HamburgerMenu";
 const Navbar = async () => {
   const categories = await getCategories(); 
  
@@ -13,14 +12,38 @@ const Navbar = async () => {
   return ( 
     <div className="border-b">
       <Container>
-        <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
-          <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-            <p className="font-bold text-xl">RoundaStore</p>
-          </Link>
+      <header className="bg-white">
+  <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
+    <a className="block text-teal-600" href="/">
+      <span className="sr-only">Home</span>
+      <Image
+        priority={true}
+        src="/logo.jpg"  // Path to your logo in the "public" directory
+        alt="Logo Alt Text"
+        width={50}       // Set the width of your logo
+        height={50}       // Set the height of your logo
+      />
+    </a>
+
+    <div className="flex flex-1 items-center justify-end md:justify-between">
+      <nav aria-label="Global" className="hidden md:block">
+        <ul className="flex items-center gap-6 text-sm">
           <MainNav data={categories} />
+        </ul>
+      </nav>
+      <div> 
           <SearchBar />
-          <NavbarActions />
         </div>
+      <div className="flex items-center gap-4">
+        <div className="sm:flex sm:gap-4">
+        <NavbarActions />
+        </div>
+        
+        <HamburgerMenu data={categories} />
+      </div>
+    </div>
+  </div>
+</header>
       </Container>
     </div> 
   );
