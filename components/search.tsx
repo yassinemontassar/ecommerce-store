d9bfdf3397f3aad2de86304a2c82c0ab59fa5172
yassinemontassar <sawthegamer70@gmail.com>
2024-01-13 23:26:25 +0100

    search componenet update

--- components/search.tsx
+++ components/search.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import { useRouter, useSearchParams } from "next/navigation";
 import Button from './ui/button';
+import { Search, SearchIcon } from 'lucide-react';
 const SearchBar: React.FC = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const router = useRouter();
@@ -33,14 +34,15 @@ const SearchBar: React.FC = () => {
         value={searchQuery}
         onChange={handleInputChange}
         // onKeyPress={handleKeyPress}
-        className=" flex items-center border border-gray-300 px-4 py-2 rounded-l-md focus:outline-none focus:ring focus:border-blue-300"
+        className=" flex items-center border border-gray-300 px-4 py-1 rounded-full focus:outline-none focus:ring focus:border-blue-300"
       />
-      <Button
+      <Search
+      size={35}
         onClick={handleSearchClick}
-        className="flex items-center bg-black text-white px-4 py-2 rounded-full focus:outline-none focus:ring focus:border-blue-300 ml-2"
-      >
-        Search
-      </Button>
+        // className="flex items-center bg-black text-white px-4 py-2 rounded-full focus:outline-none focus:ring focus:border-blue-300 ml-2"
+        className="rounded-full bg-gray-300 p-1 shadow-md hover:shadow-lg transition duration-300 cursor-pointer text-black"
+      />
+
     </div>
   );
 };
