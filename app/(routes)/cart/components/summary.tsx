e41e50e047ef2070b1f6d12a0eea6689355bb0c7
yassinemontassar <sawthegamer70@gmail.com>
2024-01-13 22:33:21 +0100

    added navbar menu to categories

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -94,6 +94,7 @@ const Summary = () => {
   return (
     
     <div className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
+
       <h2 className="text-lg font-medium text-gray-900">Order summary</h2>
 
       <div className="mt-6 space-y-4">
--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,18 +1,50 @@
 "use client";
 
-import Link from "next/link"
-import { usePathname } from "next/navigation";
-
-import { cn } from "@/lib/utils"
+import Link from "next/link";
+import { usePathname } from "next//navigation";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { Category } from "@/types";
+import { cn } from "@/lib/utils";
 
 interface MainNavProps {
   data: Category[];
 }
 
-const MainNav: React.FC<MainNavProps> = ({
-  data
-}) => {
+interface NavLinkProps {
+  route: {
+    href: string;
+    label: string;
+    active: boolean;
+  };
+}
+
+const NavLink: React.FC<NavLinkProps> = ({ route }) => (
+  <Link
+    href={route.href}
+    className={cn(
+      "text-sm font-medium transition-colors hover:text-black",
+      route.active ? "text-black" : "text-neutral-500"
+    )}
+    onClick={() => {
+      // Close the dropdown menu when a link is clicked
+      const dropdownMenu = document.querySelector('.dropdown-menu');
+      if (dropdownMenu) {
+        dropdownMenu.classList.remove('show');
+      }
+    }}
+  >
+    {route.label}
+  </Link>
+);
+
+const MainNav: React.FC<MainNavProps> = ({ data }) => {
   const pathname = usePathname();
 
   const routes = data.map((route) => ({
@@ -22,23 +54,29 @@ const MainNav: React.FC<MainNavProps> = ({
   }));
 
   return (
-    <nav
-      className="mx-6 flex items-center space-x-4 lg:space-x-6"
-    >
-      {routes.map((route) => (
-        <Link
-          key={route.href}
-          href={route.href}
-          className={cn(
-            'text-sm font-medium transition-colors hover:text-black',
-            route.active ? 'text-black' : 'text-neutral-500'
-          )}
-        >
-          {route.label} 
-      </Link>
-      ))}
-    </nav>
-  )
+    <DropdownMenu>
+      <DropdownMenuTrigger className="dropdown-menu-trigger">
+        <div className="bg-gradient-to-r from-gray-800 to-gray-600 hover:from-gray-600 hover:to-gray-800 text-white font-bold py-2 px-4 rounded shadow focus:outline-none active:bg-gray-700">
+          Boutique
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="dropdown-menu">
+        <DropdownMenuLabel>
+          <div className="flex flex-col items-center space-y-2 lg:space-y-4 mx-6">
+            Categories
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <nav className="flex flex-col items-start space-y-2 lg:space-y-4">
+            {routes.map((route) => (
+              <NavLink key={route.href} route={route} />
+            ))}
+          </nav>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
 
 export default MainNav;
