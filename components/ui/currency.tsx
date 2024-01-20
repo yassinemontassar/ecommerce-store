8b33cd40b2212a5fa9e4825aa3b43d65c77b216c
yassinemontassar <sawthegamer70@gmail.com>
2024-01-20 21:03:44 +0100

    TND currency fixes

--- components/ui/currency.tsx
+++ components/ui/currency.tsx
@@ -26,8 +26,16 @@ const Currency: React.FC<CurrencyProps> = ({
 
   const formattedValue = formatter.format(Number(value));
   const currencySymbol = formattedValue.replace(/[0-9.,]/g, '').trim();
-  const numericValue = formattedValue.replace(/[^0-9.,]/g, '').trim();
+  let numericValue = formattedValue.replace(/[^0-9.,]/g, '').trim();
 
+  // Check if the numeric value is an integer
+  const isInteger = Number.isInteger(Number(numericValue));
+
+  // Remove trailing zeros if it's an integer
+  if (isInteger) {
+    numericValue = String(Number(numericValue));
+  }
+  numericValue = numericValue.replace('.', ',');
   return ( 
     <div className="font-semibold">
       {numericValue} {currencySymbol}
