6d649d94f9f559a1d6f00aa5b1fdbad9cff2a385
yassinemontassar <sawthegamer70@gmail.com>
2024-01-23 21:48:05 +0100

    Dynamic Sitemap and robot.tsx

---
+++ app/sitemap.ts
@@ -0,0 +1,25 @@
+import getProducts from "@/actions/get-products";
+import { MetadataRoute } from "next";
+
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+    const products = await getProducts();
+
+    const productEntries: MetadataRoute.Sitemap = products.map(({id}) => ({
+        url: `${process.env.NEXT_PUBLIC_BASE_URL}/product/${id}}`,
+        priority: 0.8,
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+       
+    }))
+    return [
+        {
+            url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
+            priority: 1,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+        },
+        ...productEntries,
+    ]
+    
+}
